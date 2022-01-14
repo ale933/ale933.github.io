e5937dda8aec68e81a45eb3f2b634cc354b26230
ale933 <73939749+ale933@users.noreply.github.com>
2022-01-14 15:37:56 +0100

    scriptt

--- Altro giochino/script.js
+++ Altro giochino/script.js
@@ -4,11 +4,11 @@ var scoree = 1;
 
 
 
-
 function pene() {
-    var randomTop = Math.floor(Math.random() * 480);
+    var randomTop = Math.floor(Math.random() * 470);
     var randomLeft = Math.floor(Math.random() * 100);
     document.getElementById('block').style.top = `${randomTop}px`;
     document.getElementById('block').style.left = `${randomLeft}%`;
     document.getElementById('score').innerHTML = 'Score: ' + scoree++;
 }
+
